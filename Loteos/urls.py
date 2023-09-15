96a7f375a46681c519a6fb3f2dc42f0c70dc0d02
Mostrosalvado <diealesal@gmail.com>
2023-09-15 17:18:38 -0300

    funciona la busqueda de lotes por id  y agregue un toque de css

--- Loteos/urls.py
+++ Loteos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loteos/', views.Lote, name='lote'),
+    path('loteos/', views.Loteo, name='lote'),
+    path('buscar_lote/<int:id_lote>/', views.BuscaLote, name='Buscar_lote'),
 ]
--- Loteos/views.py
+++ Loteos/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Lote
+#from django.shortcuts import get_object_or_404
+
+
+
 
 # Create your views here.
-def Lote (request):
-        numeros= range(1,103)
+def Loteo (request):
         return render(request, "Lotes.html",{
         'title':'Lotes',
-        'numeros':numeros
 })
 
+
+
+def BuscaLote(request, id_lote=None):  
+        if request.method == 'POST':
+                id_lote = request.POST.get('id_lote')
+                lote_encontrado = get_object_or_404(Lote, id_lote=id_lote)
+                
+
+                return render(request, 'Buscar_lote.html', {'lote_encontrado': lote_encontrado})
+        else:
+                # Si no es una solicitud POST, muestra el formulario vacío
+                return render(request, 'Lotes.html')
